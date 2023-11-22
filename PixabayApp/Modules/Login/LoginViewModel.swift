21ce0337721893180aa4b35b82157a3c4dde2fad
Ibrohim <ibrohim625m@gmail.com>
2023-11-22 14:55:32 +0500

    Add RxSwift bindings to LoginViewController

---
+++ PixabayApp/Modules/Login/LoginViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewModel.swift
+//  PixabayApp
+//
+//  Created by Ibrokhim Movlonov on 22/11/23.
+//
+
+import Foundation
+
+class LoginViewModel {
+    
+    private let coordinator: Coordinator
+    
+    init(coordinator: Coordinator) {
+        self.coordinator = coordinator
+    }
+    
+    // MARK: -
+
+}
