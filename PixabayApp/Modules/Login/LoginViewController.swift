c34d4349fca8f6e985ae66cfd88e3bdab6ad7bf7
Ibrohim <ibrohim625m@gmail.com>
2023-11-22 12:31:37 +0500

    Create Login Module

---
+++ PixabayApp/Modules/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  PixabayApp
+//
+//  Created by Ibrokhim Movlonov on 22/11/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
